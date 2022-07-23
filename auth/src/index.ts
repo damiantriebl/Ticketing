0669e8727a2699d534ddda34b3531c195c44c2a6
Damian <demiantriebl@gmail.com>
2022-07-23 14:28:39 -0300

    last Changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import mongoose   from 'mongoose';
 
 
 const start = async () =>{
+  console.log('starting up...')
   if(!process.env.JWT_KEY){
     throw new Error('you need to specify the jwt secret')
   }

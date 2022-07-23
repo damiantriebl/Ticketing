af9d11c71b1b3275e47b50d96b516785784e8e42
Damian <demiantriebl@gmail.com>
2022-07-23 14:30:49 -0300

    add dev

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose   from 'mongoose';
 
 
 const start = async () =>{
-  console.log('starting up...')
+  console.log('starting up... dev ');
   if(!process.env.JWT_KEY){
     throw new Error('you need to specify the jwt secret')
   }
